0b3b76e1824dc96e471ec2e109e09dd722a31a88
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-09-10 21:41:10 +0000

    second scrape

--- lib/reddit_now/posts.rb
+++ lib/reddit_now/posts.rb
@@ -6,23 +6,29 @@ class RedditNow::Posts
   end
   
   def self.scraped
-    puts "1. 28.5k General from black panther I shall see - by gallowboob" #"#{i}. #{title} by #{user}"
+    #puts "1. 28.5k General from black panther I shall see - by gallowboob" #"#{i}. #{title} by #{user}"
     posts = [] 
     
     posts << self.scraped_frontpage
+    posts 
+    puts "#{posts.rank}. #{posts.upvote} Upvotes. #{posts.title} - submitted to #{posts.subreddit} by #{posts.user}"
   end
   
   def self.scraped_frontpage
-    list = []
     page = Nokogiri::HTML(open("https://old.reddit.com/r/all/", 'User-Agent' => 'Nooby'))
+    
     posts = page.css("div[data-author]")
-    binding.pry
-    user = posts.css("p.tagline").css("a").first.text
-    subreddit = posts.css("p.tagline").css("a")[1].text
-    title = posts.css("p.title").css("a").first.text
-    rank = posts.css("span.rank").first.text
-    url = posts.css("p.title").css("a").first.attr("href")
-    upvote = posts.css(".dislikes").first.text
+    posts.each do |op|
+      new_post = self.new
+      new_post.user = op.css("p.tagline").css("a").first.text
+      new_post.subreddit = op.css("p.tagline").css("a")[1].text
+      new_post.title = op.css("p.title").css("a").first.text
+      new_post.rank = op.css("span.rank").first.text
+      new_post.url = op.css("p.title").css("a").first.attr("href")
+      new_post.upvote = op.css(".dislikes").first.text
+  
+      new_post
+    end
     
   end
   
--- lib/reddit_now/scraper.rb
+++
@@ -1,4 +0,0 @@
-class RedditNow::Scraper
-  
-  
-end
